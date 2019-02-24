57fab05f310eea850aa48bf81fdea948680647aa
ronizoller <ronizo@post.bgu.ac.il>
2019-02-24 10:47:48 +0200

    19.2

--- PythonCode/test.py
+++ PythonCode/test.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.append('/anaconda3/lib/python3.6/site-packages')
 import matplotlib.pyplot as plt
 
-times_effi = {200: 0.058, 300: 0.1227, 250: 0.0976, 100: 0.012, 150: 0.0338}
-times_naive = {200: 0.0679, 300: 0.2384, 250: 0.1364, 100: 0.0053, 150: 0.026}
+path = '/Users/ronizoller/Documents/school/Master/מחקר/DATA/'
+
+times_effi = {200: 0.058, 300: 0.1227, 250: 0.0976, 100: 0.012, 150: 0.0338,400: 0.2293, 450: 0.2757, 500: 0.328, 550: 0.4594, 350: 0.1812}
+times_naive = {200: 0.0679, 300: 0.2384, 250: 0.1364, 100: 0.0053, 150: 0.026,400: 0.5899, 450: 0.8389, 500: 1.1318, 550: 1.5775, 350: 0.3938}
 
 
 xs = []
@@ -13,10 +17,11 @@ for number_of_leafs,time in times_effi.items():
     effi.append(time)
 for number_of_leafs,time in times_naive.items():
     naive.append(time)
+fig, ax = plt.subplots()
 plt.xlabel('Number of Leafs', fontsize=10)
 plt.ylabel('Running Time', fontsize=10)
-fig, ax = plt.subplots()
 plt.plot(xs, effi, 'ro')
 plt.plot(xs, naive, 'g^')
 plt.axis([0, max(xs)+50, 0, max(effi+naive)+0.5])
-plt.show()
+fig = ax.get_figure()
+fig.savefig(path + '/plot_noise.png')
