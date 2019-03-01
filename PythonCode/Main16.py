6b1fc50dfb317e3b052f6cb8015f5f4c4adab8ea
ronizo <ronizo@post.bgu.ac.il>
2019-03-01 15:09:24 +0200

    done plotting compare

--- PythonCode/Main16.py
+++ PythonCode/Main16.py
@@ -34,7 +34,7 @@ glob = False                                        # if True global alignment i
 compare_subtrees = False                             # if true the algorithm will look for a signi different between two children of u in G, otherwise it will look for u in G s.t. in G(u) there are alot of same color HT
 dis_flag = True                                     #count the patterns and take in count the distance of the HT
 one_enriched_on_not = False
-k = 100
+k = 500
 exact_names = True
 
 evolutinary_event = 'HT'
